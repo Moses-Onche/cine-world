92cbf7652b2552e455bba9bf7654ec27f9ed32dc
Moses-Onche <onchemoses38@gmail.com>
2023-11-07 18:15:50 +0100

    Update style in  app.css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+import { React, useEffect } from 'react';
 import './App.css';
 
+const API_URL = 'http://www.omdbapi.com?apikey=27e92d25'
+
 function App() {
+
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json()
+
+    console.log(data)
+  }
+
+  useEffect(() => {
+    searchMovies('Iron man');
+  }, [])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Let's build this</h1>
     </div>
   );
 }
